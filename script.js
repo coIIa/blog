7ceeff7eb7621adc974f0cb37d35094a64af89bf
coIIa <gabriel.colla16@escola.pr.gov.br>
2026-08-18 09:51:46 -0300

    dKSHDBA

--- script.js
+++ script.js
@@ -3,17 +3,16 @@ const botoes = document.querySelectorAll('button');
 botoes.forEach(botao => {
    botao.addEventListener('click', curtir) 
    let curtiu = false
-
-
-
    function curtir() {
-    let curtidas = document.querySelector('span')
-    if (curtiu == false) {
-      curtiu = true
+    let curtidas = botao.querySelector('span')
+    if(curtiu == false){
+       curtiu = true
       curtidas.textContent++
-      botao.style.backgroundColor = 'blue'
+      botao.style.backgroundColor = 'white'
+    }else{
+      curtiu = false
+      curtidas.textContent--
+      botao.style.backgroundColor = 'white'
     }
-
-
    }
 });
